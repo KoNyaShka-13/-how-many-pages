98f3e2a444dbc3827d3775c8f2c061bfa36a5798
KoNyaShKa <ggiziawpgg@gmail.com>
2023-12-05 20:43:19 +0300

    =

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+function amountOfPages(summary){
+    let result = [];//Создаем массив для более легкого счета
+
+for (let i = 1;result.join("").length!=summary;i++){//Считаем, пока небудут равны показатели
+result.push(i);
+}
+
+
+console.log(result.at(-1))
+
+}
+amountOfPages(11);
+  
+
+
